fd83e010d924c99f21c01a28371d493e1b13405b
Genaro Gonzalez <genarrogg@gmail.com>
2026-05-31 19:32:15 -0400

    publicando

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,7 @@
 // Let's create a local copy of the func directory inside lib!
 
 // For now, let's just use the same imports but we'll adjust them later!
-import initMoneyInputs from '../frontend/src/func/inputMoney/index.js';
-import { showMoney } from '../frontend/src/func/math/index.js';
+import initMoneyInputs from '../frontend/src/func/inputMoney/index';
+import { showMoney, moneyToString, montoNoNegativo } from '../frontend/src/func/index';
 
-export { initMoneyInputs, showMoney };
+export { initMoneyInputs, showMoney, moneyToString, montoNoNegativo };
