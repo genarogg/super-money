d02fb1fd031ae1af022ff69928bee0c4825b658a
Genaro Gonzalez <genarrogg@gmail.com>
2026-08-27 18:40:58 -0400

    fix infinito

--- frontend/src/func/montoNoNegativo.ts
+++ frontend/src/func/montoNoNegativo.ts
@@ -2,11 +2,11 @@ const montoNoNegativo = (valor?: number) => {
     if (valor === undefined) {
         return { isValido: true };
     };
-    if (isNaN(valor) || valor < 0) {
+    if (!Number.isFinite(valor) || valor < 0) {
         return { isValido: false, message: "El monto no puede ser negativo" };
     }
-
+ 
     return { isValido: true };
 };
-
+ 
 export default montoNoNegativo;
